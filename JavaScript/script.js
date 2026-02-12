60543d44ca369fb814e8f4184e389a89e485a3d4
Ashish Raj <rajlakra5277@gmail.com>
2026-02-12 10:16:04 +0530

    Intro JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+// console.log("ashish");
+// console.info("raj");
+// console.warn("warn");
+// console.error("error");
+// console.table();
+
+// let nm = prompt("name ?");
+// console.log(nm);
+
+// alert("alert");
