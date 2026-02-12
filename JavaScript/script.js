db82081005b3d379cc325bf288b2a781a3142605
Ashish Raj <rajlakra5277@gmail.com>
2026-02-12 10:48:03 +0530

    js basic

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,3 +8,24 @@
 // console.log(nm);
 
 // alert("alert");
+
+        // day  -  28
+
+// how to add comments
+//datatypes - types of data
+    // ashish - String
+    // 12 - Number
+    // 73.38 - float
+    // null char array object boolean etc
+
+// undifined
+    // var a;
+
+//NaN
+    // koi operation ka number mai val nhi hai to phir - NaN aaiyega
+
+// relative and primitive datatypes in js
+    // var a = [1,2,3,4];
+    // var s = a; // s mai a ka reference gaya hai copy nhi huwa hai
+    // reference - array object function - direct value can't copy
+    // primitives - number boolean strings null undefined NaN - direct value can copy
