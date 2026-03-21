f6768f1b7432feea8f68b3ccad3cf1ac5d1d1a70
Ashish Raj <rajlakra5277@gmail.com>
2026-03-21 16:48:20 +0530

    DOM - random ele on click

--- JavaScript/DOM/script.js
+++ JavaScript/DOM/script.js
@@ -22,19 +22,42 @@
 
         // day - 43
 
+// var btn = document.querySelector('button')
+// var box = document.querySelector('#box')
+
+// btn.addEventListener('click',function(){
+//     var c1 = Math.floor(Math.random()*256);
+//     var c2 = Math.floor(Math.random()*256);
+//     var c3 = Math.floor(Math.random()*256);
+
+//     box.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+// })
+
+// var arr = ['ashish','rahul','mohit','shivam','arnav','atishey','sumit','aadarsh','pragati','samit']
+
+// var a = Math.floor(Math.random()*arr.length)
+// console.log(arr[a]);
+
+
+        // day - 44
+
 var btn = document.querySelector('button')
-var box = document.querySelector('#box')
+var main = document.querySelector('main')
 
 btn.addEventListener('click',function(){
-    var c1 = Math.floor(Math.random()*256);
-    var c2 = Math.floor(Math.random()*256);
-    var c3 = Math.floor(Math.random()*256);
-
-    box.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+        var div = document.createElement('div')
+        var x = Math.floor(Math.random()*90)
+        var y = Math.floor(Math.random()*90)
+        var r = Math.floor(Math.random()*100)
+        var c1 = Math.floor(Math.random()*256)
+        var c2 = Math.floor(Math.random()*256)
+        var c3 = Math.floor(Math.random()*256)
+        div.style.height = '70px'
+        div.style.width = '70px'
+        div.style.position = 'absolute'
+        div.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+        div.style.left = x+'%'
+        div.style.top = y+'%'
+        div.style.rotate = r+'deg'
+        main.appendChild(div)
 })
-
-var arr = ['ashish','rahul','mohit','shivam','arnav','atishey','sumit','aadarsh','pragati','samit']
-
-var a = Math.floor(Math.random()*arr.length)
-
-console.log(arr[a]);
