fc23ce84146a323c6bc9d9ef24e56b1f5bd07649
Ashish Raj <rajlakra5277@gmail.com>
2026-03-26 22:39:57 +0530

    dynamic ui using JS

--- JavaScript/DOM/script.js
+++ JavaScript/DOM/script.js
@@ -101,19 +101,60 @@
 
         // day - 46
 
-var img = document.querySelector('img')
-var love = document.querySelector('#love')
+// var img = document.querySelector('img')
+// var love = document.querySelector('#love')
 
-img.addEventListener('dblclick',function(){
-        love.style.opacity = 1
-        love.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)'
-        setTimeout(() => {
-                love.style.transform = 'translate(-50%,-350%) scale(1) rotate(60deg)'
-        }, 800);
-        setTimeout(() => {
-                love.style.opacity = 0
-        }, 1000);
-        setTimeout(() => {
-                love.style.transform = 'translate(-50%,-50%) scale(0) rotate(-60deg)'
-        }, 1200);
+// img.addEventListener('dblclick',function(){
+//         love.style.opacity = 1
+//         love.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)'
+//         setTimeout(() => {
+//                 love.style.transform = 'translate(-50%,-350%) scale(1) rotate(60deg)'
+//         }, 800);
+//         setTimeout(() => {
+//                 love.style.opacity = 0
+//         }, 1000);
+//         setTimeout(() => {
+//                 love.style.transform = 'translate(-50%,-50%) scale(0) rotate(-60deg)'
+//         }, 1200);
+// })
+
+                // day - 47
+
+const users = [
+  {
+    username: "ashish_dev",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    profession: "Frontend Developer",
+    description: "Passionate about building interactive UI using React and JavaScript."
+  },
+  {
+    username: "anyone_codes",
+    image: "https://randomuser.me/api/portraits/women/2.jpg",
+    profession: "Backend Developer",
+    description: "Loves working with APIs, databases, and server-side logic."
+  },
+  {
+    username: "random_ui",
+    image: "https://randomuser.me/api/portraits/women/3.jpg",
+    profession: "UI/UX Designer",
+    description: "Designs clean and user-friendly interfaces with great user experience."
+  },
+  {
+    username: "rahul_ai",
+    image: "https://randomuser.me/api/portraits/men/4.jpg",
+    profession: "AI Engineer",
+    description: "Works on machine learning models and intelligent systems."
+  }
+];
+
+var sum = ''
+users.forEach(function(ele){
+        sum += `<div class="card">
+                <img src="${ele.image}" alt="">
+                <h2>${ele.username}</h2>
+                <h3>${ele.profession}</h3>
+                <p>${ele.description}</p>
+        </div>`
 })
+var main = document.querySelector('main')
+main.innerHTML = sum
