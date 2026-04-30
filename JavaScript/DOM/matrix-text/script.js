95b172f4aba9a01426543d5ec00628d39feff73d
Ashish Raj <rajlakra5277@gmail.com>
2026-05-01 01:55:08 +0530

    Matrix-style text scramble

--- JavaScript/DOM/matrix-text/script.js
+++ JavaScript/DOM/matrix-text/script.js
@@ -1,12 +1,17 @@
 const para = document.querySelector('p')
 const chara = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 const text = para.innerHTML
+let iteration = 0
 para.addEventListener('mouseenter',() => {
     
     setInterval(() => {
-        const str = text.split('').map((ele,idx) => {
-        return chara.split('')[Math.floor(Math.random()*53)]
+        const str = text.split("").map((ele,idx) => {
+            if(idx < iteration){
+                return ele
+            }
+        return chara.split("")[Math.floor(Math.random()*52)]
         }).join("")
         para.innerHTML = str;
-    }, 50);
+        iteration += 0.30
+    }, 30);
 })
