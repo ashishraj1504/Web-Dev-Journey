8a7eb8390c895e229fd37bc95f9780a6b4094145
Ashish Raj <rajlakra5277@gmail.com>
2026-05-07 22:59:04 +0530

    day-59 adv JS

--- JavaScript/Advance/script.js
+++ JavaScript/Advance/script.js
@@ -1,13 +1,36 @@
-class Person{
-    constructor(name,age,email){
-        this.name = name
-        this.age = age
-        this.email = email
-    }
-}
+// class Person{
+//     constructor(name,age,email){
+//         this.name = name
+//         this.age = age
+//         this.email = email
+//     }
+// }
 // prototype - shared memory
-Person.prototype.course = "btech"
+// Person.prototype.course = "btech"
 
-let p1 = new Person("ashish",20,"e@h.com")
-let p2 = new Person("rahul",20,"r@g.com")
-let p3 = new Person("mohit",20,"m@f.com")
+// let p1 = new Person("ashish",20,"e@h.com")
+// let p2 = new Person("rahul",20,"r@g.com")
+// let p3 = new Person("mohit",20,"m@f.com")
+
+// call -> fnc chalata hai and this ki value set karta hai
+//apply -> WAHI KARTA HAI JO CALL KARTA HAI AND arguments mein pahli value this ki and doosri value array hoti hai
+// blind -> wahi karta hai jo call karta hai and aapko naya fnc deta hai
+
+    //callback -> ek func jo turant nhi chalega ye chalega jab aapka koi kaam complete hoga
+
+// setTimeout(function(){
+//     console.log("hey");
+// },2000)
+
+// function UserAddress(address,cb) {
+//     console.log("fetching Details: ");
+//     setTimeout(()=>{
+//         cb({
+//             city: "KP3",
+//             state: "U.P"
+//         })
+//     },2000)
+// }
+// UserAddress("provide address", function(Details){
+//     console.log(Details);
+// })
