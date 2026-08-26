c1b481c205e254a97f9374cd0216e8b1939f3154
Ashish Raj <rajlakra5277@gmail.com>
2026-08-27 01:05:40 +0530

    day 63 overview about fetch

--- JavaScript/Advance/script.js
+++ JavaScript/Advance/script.js
@@ -64,7 +64,6 @@
 // }
 // abc();
 
-
 // function getnum() {
 //     return new Promise((reslove, reject)=>{
 //         setTimeout(() => {
@@ -87,7 +86,7 @@
 //     day - 62
 
 // syntax error -> written code mai error
-// runtime error -> no error at time of writting code 
+// runtime error -> no error at time of writting code
 // logical error -> diff work instead of some work
 
 // try {
@@ -105,3 +104,60 @@
 // } catch (err) {
 //     console.log(new Error("something went wrong"));
 // }
+
+//     day - 63
+
+// async function getWeather(city){
+//     try{
+//         let apikey = `f83cb56ca2aefc2812b076c93255dd4c`;
+
+//     let raw = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&exclude={part}&appid=${apikey}`);
+
+//     if(!raw.ok){
+//         throw new Error("location name is wrong");
+//     }
+
+//     let realData = await raw.json();
+//     console.log(realData);
+
+//     }
+//     catch(err){
+//         console.log(err.message);
+//     }
+
+// }
+// getWeather("london");
+
+const users = [
+    "a.gmail.com",
+    "b.gmail.com",
+    "c.gmail.com"
+];
+function sendEmail(email) {
+  return new Promise((resolve, reject) => {
+    let time = Math.floor(Math.random() * 5);
+    setTimeout(() => {
+      let prob = Math.floor(Math.random() * 10);
+      if (prob <= 5) resolve("Email sent");
+      else reject("email failed");
+    }, time * 1000);
+  });
+}
+
+async function sendEmails(email) {
+  let allRepo = email.map(function (email) {
+    return sendEmail(email)
+      .then(function (data) {
+        return data;
+      })
+      .catch(function (err) {
+        return err;
+      });
+  });
+  let ans = await Promise.all(allRepo);
+
+  ans.forEach(function (status) {
+    console.log(status);
+  });
+}
+sendEmails(users);
